c826db598b3c126f49116f8154c9778f53710321
Rowan-Hyeho-Song <dev.rowansong@gmail.com>
2024-01-31 17:42:52 +0900

    Update README.md
    readme에 메모할 내용 작성

--- src/index.js
+++ src/index.js
@@ -5,13 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// React.StrictMode > 렌더링 두번 발생 거슬리면 제거
 root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
