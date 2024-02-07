b4a3be59244bc41c766a71670f7b85135070fdc9
Rowan-Hyeho-Song <dev.rowansong@gmail.com>
2024-02-07 14:15:58 +0900

    Create PortfolioList.js
    따로 포트폴리오 파일을 만들어서 아이템을 관리하도록 관리 파일 생성

---
+++ src/constants/PortfolioList.js
@@ -0,0 +1,42 @@
+/**
+ * 추후 portfolio 추가시 path 및 일부 아이템 컬러 지정해서 나오도록 설정
+ */
+const colors = [
+    '#90211B', '#A82921', '#C13229',
+    '#3E379E', '#473FAA', '#4E47B4',
+    '#1C7D93', '#1A788F', '#1B7A90',
+    '#567C24', '#67982A', '#80BF32',
+    '#BCA821', '#CFB823', '#EBCE13'
+];
+const emptyColor = ['#303030', '#333333', '#3E3E3E'];
+const empty = {
+    title: "..."
+};
+const datas = [
+    {
+        title: "Test-01"
+    },
+    {
+        title: "Test-02"
+    },
+    {
+        title: "Test-03"
+    },
+    {
+        title: "Test-04"
+    },
+    {
+        title: "Test-05"
+    },
+    {
+        title: "Test-06"
+    }
+];
+const PortfolioList = Array.from({ length: Math.max(datas.length, 15) }, (_, i) => {
+    if (i < datas.length) {
+        return { ...datas[i], key: i, color: colors[i % colors.length] };
+    }
+    return { ...empty, color: emptyColor[i % emptyColor.length], key: i };
+});
+
+export default PortfolioList;
