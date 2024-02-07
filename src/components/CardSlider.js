cc275d4c7b59fb35d5b7c2e81b3a832915247c73
Rowan-Hyeho-Song <dev.rowansong@gmail.com>
2024-02-07 14:16:46 +0900

    Update CardSlider.js
    슬라이더의 아이템이 현재 focus되어있는 아이템을 기준으로 좌우로 자동 배치되도록 수정 (infinity scroll)

--- src/components/CardSlider.js
+++ src/components/CardSlider.js
@@ -1,60 +1,126 @@
 import styled from 'styled-components';
 import { useState, useEffect } from 'react';
-import { useWindowSize } from 'hooks/useWidowSize';
-import { throttle, debounce } from 'lodash';
 
 const Container = styled.div`
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    border: 1px solid red;
+position: relative;
+display: flex;
+justify-content: center;
+align-items: center;
+width: 100%;
+height: 100%;
+border: 1px solid red;
 `;
 
-const CardWrapper = styled.ul`
-    list-style-type: none;
-    transition: transform .1s;
-    
-    display: flex;
-    justify-content: center;
-    align-items: center;
+const CardWrapper = styled.ul.attrs((props) => {
+    const { position, innerRadius, rotate } = props;
+    const [x, y] = position.split(" ");
+    const pos = ["0px", "0px"];
+    switch(x) {
+        case "left": pos[0] = `calc(-50vw - ${innerRadius})`; break;
+        case "right": pos[0] = `calc(50vw + ${innerRadius})`; break;
+        default: break;
+    };
+    switch(y) {
+        case "top": pos[1] = `calc(-50vh - ${innerRadius})`; break;
+        case "bottom": pos[1] = `calc(50vh + ${innerRadius})`; break;
+        default: break;
+    };
 
-    transform-origin: center center;
-    transform: translateY(1300px) rotate(${({ rotate }) => rotate}deg);
-    background-color: red;
-    width: 100px;
-    height: 100px;
-    margin: 0;
-    padding: 0;
-`;
-
-const Card = styled.li`
-    position: absolute;
-    width: 30vh;
-    height: 40vh;
-    background-color: #00000022;
-    border: 1px solid #333333;
-
-    text-align: center;
-    font-weight: 600;
-    z-index: 0;
-
-    cursor: pointer;
-    transform-origin: center center;
-
-    &:hover {
-        background-color: #FF555522;
-        z-index: 2;
+    return { 
+        style: {
+            transform: `translate(${pos.join(",")}) rotate(${rotate}deg)`
+        }
     }
+})`
+list-style-type: none;
+transition: transform .1s;
+
+display: flex;
+justify-content: center;
+align-items: flex-end;
+
+transform-origin: center center;
+margin: 0;
+padding: 0;
+`;
+
+const Card = styled.li.attrs((props) => ({
+    backgroundColor: props.color
+}))`
+position: absolute;
+width: 20vh;
+height: 27vh;
+
+text-align: center;
+font-weight: 600;
+z-index: 0;
+
+cursor: pointer;
+transform-origin: center bottom;
+color: white;
+
+-webkit-user-select:none;
+-moz-user-select:none;
+-ms-user-select:none;
+user-select:none;
+
+&.focus, &:hover {
+    z-index: 2;
+}
 `;
 
 function CardSlider (props) {
-    const { items } = props;
+    const { 
+        items, 
+        innerRadius = "0px",
+        floatViewport = "50%" // Card 크기 기준
+    } = props;
 
-    const windowSize = useWindowSize();
-    const [currentArc, setCurrentArc] = useState(0);
+    const [curArc, setCurArc] = useState(0);
+    const [curFocus, setCurFocus] = useState(0);
+    const [cardList, setCardList] = useState([]);
+
+    const itemSize = items.length
+    const translate = `calc(-${innerRadius} - ${floatViewport})`;
+    
+    useEffect(() => {
+        const point = itemSize / 2;
+        const angle = 8;
+        
+        const cards = items.map((item) => {
+            const order = item.key - curFocus;
+            const preOrder = item.order;
+            const newOrder = order > point ? order - itemSize : 
+                            order < -point ? order + itemSize : order;
+            item.order = newOrder;
+            item.translate  = translate;
+            if (item.rotate === undefined) {
+                item.rotate = item.order * angle;
+            } else{
+                if ((preOrder !== 0 && newOrder !== 0) && preOrder + newOrder === 0) {
+                    const rotates = items.map((item) => item.rotate);
+                    if ( newOrder >= 0 ) {
+                        item.rotate = Math.max(...rotates) + angle;
+                    } else {
+                        item.rotate = Math.min(...rotates) - angle;
+                    }
+                }
+            }
+            return item;
+        });
+        cards.sort((a, b) => a.order - b.order);
+        setCardList(cards);
+    }, [curFocus, itemSize, items, translate]);
+
+    useEffect(() => {
+        cardList.some((card) => {
+            if (Math.abs(card.rotate + curArc) % 360 < 5) {
+                setCurFocus(card.key);
+                return true;
+            }
+            return false;
+        });
+    }, [curArc, cardList]);
 
     useEffect(() => {
         const target = document.getElementById("slider-container");
@@ -79,8 +145,7 @@ function CardSlider (props) {
 
                 const rad = Math.atan2(vectorECY, vectorECX) - Math.atan2(vectorSCY, vectorSCX);
                 const deg = rad * (180 / Math.PI);
-
-                setCurrentArc((currentArc) => currentArc + deg);
+                setCurArc((curArc) => curArc + deg);
                 startX = endX;
                 startY = endY;
             };
@@ -95,25 +160,28 @@ function CardSlider (props) {
         return () => {
             target.removeEventListener("mousedown", bindRotateCardEvent);
         };
-    }, [currentArc])
-
-    const CardList = () => {
-        const cards = Array.from({ length: items }, (_, i) => i + 1);
-        const point = items / 2;
-        const angle = 13;
-        const list = cards.map((card, i) => {
-            // const translate = '-300px';
-            const translate = 'calc(-75vw + 20vh)';
-            const rotate = i < point ? i * angle : (items - i) * -angle;
-            return (<Card className="no-drag" style={{ transform: `rotate(${rotate}deg) translateY(${translate})`}}>{card}</Card>);
-        });
-        return <>{list}</>;
-    }
+    }, [curArc]);
 
     return (
         <Container id="slider-container">
-            <CardWrapper id="card-wrapper" rotate={currentArc}>
-                <CardList />
+            <CardWrapper 
+                id="card-wrapper" 
+                rotate={curArc} 
+                position={"center bottom"}
+                innerRadius={innerRadius}
+            >
+                { cardList && cardList.map((card) => {
+                    return (
+                        <Card 
+                            key={card.key} 
+                            className={card.order === 0 ? "focus": ""}
+                            style={{
+                                transform: `rotate(${card.rotate}deg) translateY(${translate})`,
+                                backgroundColor: card.color
+                            }}
+                        >{card.title}</Card>
+                    );
+                })}
             </CardWrapper>
         </Container>
     );
