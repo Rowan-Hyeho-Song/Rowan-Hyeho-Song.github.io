426aa24042b12232fbeb2e2667db06dac20ea48d
Rowan-Hyeho-Song <dev.rowansong@gmail.com>
2024-03-20 19:33:32 +0900

    Update PortfolioList.js
    최신 포트폴리오가 1번에 위치하도록 순서 수정

--- src/constants/PortfolioList.js
+++ src/constants/PortfolioList.js
@@ -15,24 +15,9 @@ const empty = {
 
 const datas = [
     {
-        title: "Blog-Example",
-        date: ["JANUARY", 2020],
-        url: "https://github.com/Roar-Song/Blog-Example"
-    },
-    {
-        title: "Mini TowerDefense",
-        date: ["JANUARY", 2020],
-        url: "https://github.com/Roar-Song/MiniTowerDefense"
-    },
-    {
-        title: "Mango CRM",
-        date: ["FEBRUARY", 2020],
-        url: "https://github.com/Roar-Song/MangoCRM"
-    },
-    {
-        title: "2020 Portfolio Site",
-        date: ["FEBRUARY", 2020],
-        url: "https://roar-song.github.io/"
+        title: "Simple Cube",
+        date: ["MARCH", 2024],
+        url: "https://rowan-hyeho-song.github.io/SimpleCube/"
     },
     {
         title: "Netflix Clone Site",
@@ -40,17 +25,26 @@ const datas = [
         url: "https://rowan-hyeho-song.github.io/Netflix/"
     },
     {
-        title: "Simple Cube",
-        date: ["MARCH", 2024],
-        url: "https://rowan-hyeho-song.github.io/SimpleCube/"
-    }
+        title: "2020 Portfolio Site",
+        date: ["FEBRUARY", 2020],
+        url: "https://roar-song.github.io/"
+    },
+    {
+        title: "Mango CRM",
+        date: ["FEBRUARY", 2020],
+        url: "https://github.com/Roar-Song/MangoCRM"
+    },
+    {
+        title: "Mini Tower Defense",
+        date: ["JANUARY", 2020],
+        url: "https://github.com/Roar-Song/MiniTowerDefense"
+    },
+    {
+        title: "Blog-Example",
+        date: ["JANUARY", 2020],
+        url: "https://github.com/Roar-Song/Blog-Example"
+    },
 ];
-// const PortfolioList = Array.from({ length: Math.max(datas.length, 15) }, (_, i) => {
-//     if (i < datas.length) {
-//         return { ...datas[i], key: i, color: colors[i % colors.length] };
-//     }
-//     return { ...empty, color: emptyColor, key: i };
-// });
 
 const getPortfolioList = () => {
     const PortfolioList = Array.from({ length: Math.max(datas.length, 15) }, (_, i) => {
